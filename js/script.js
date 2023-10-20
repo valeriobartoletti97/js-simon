b5d4a9fcedc9e0cbafd4e80cab4f77cb5ca48f51
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-20 17:27:24 +0200

    added results

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ function simonSays(){
     /* variables section */
    const numbersContainer = document.getElementById('numbers-container');
    const inputContainer = document.getElementById('input-container');
+   const result = document.getElementById('result');
    const numbersToGenerate = 5;
    const generatedNumbers = [];
    let score = 0;
-   
+   let userNumbers = [];
    /* generating numbers */
    while(generatedNumbers.length < numbersToGenerate){
     let numberGenerated = getRndInteger(1, 100);
@@ -33,11 +34,32 @@ function simonSays(){
    /* button click */
    btn.addEventListener('click', function(){
     /* array of users numbers */
-    let userNumbers = []
     for (let i = 0; i < inputEl.length; i++){
         let a = parseInt(inputEl[i].value);
         userNumbers.push(a);
-    }
-    console.log(userNumbers)
-   })
+    };
+    console.log(userNumbers);
+    userScore();
+    console.log(score);
+    results();
+   });
+   
+   /* score */
+   function userScore(){
+    for(let i = 0; i < userNumbers.length; i++){
+        for(let j = 0; j < generatedNumbers.length; j++){
+            if(userNumbers[i] === generatedNumbers[j])
+            score++;
+        };
+       }; 
+   };
+
+   /* result */
+   function results(){
+    let msg = score === 0 ? '<br> Sei veramente una frana... Riprova' : score === 5 ? '<br> Hai vinto! Complimenti non ho mai visto un giocatore così forte!' : '';
+    console.log(msg)
+    result.innerHTML = 'Il tuo punteggio è : ' + score + msg;
+    inputContainer.classList.add('d-none');
+    result.classList.remove('d-none');
+   }
 };
